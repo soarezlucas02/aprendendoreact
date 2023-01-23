bbc33a2bda6b330c57636f8d0110d139e5b566ec
soarezlucas02 <lucassoaresesouza@gmail.com>
2023-01-23 17:54:46 -0300

    Acicionando Lista Suspensa

--- organo/src/componentes/CampoTexto/index.js
+++ organo/src/componentes/CampoTexto/index.js
@@ -3,8 +3,7 @@ const CampoTexto = (props) => {
     return (
         <div className="campo-texto">
             <label>{props.label}</label>
-            <input placeholder={props.placeholder
-            }/>
+            <input placeholder={props.placeholder}/>
         </div>
     )
 }
--- organo/src/componentes/Formulario/index.js
+++ organo/src/componentes/Formulario/index.js
@@ -1,6 +1,18 @@
 import CampoTexto from '../CampoTexto'
+import ListaSuspensa from '../ListaSuspensa'
 import './Formulario.css'
 const Formulario = () =>{
+    
+    const times=[
+        'Programação',
+        'Front-End',
+        'Data Science',
+        'Devops',
+        'UX e Design',
+        'Mobile',
+        'Inovação e Gestão'
+    ]
+    
     return (
         <section className='formulario'>
             <form>
@@ -8,6 +20,7 @@ const Formulario = () =>{
                 <CampoTexto label="Nome" placeholder="Digite seu nome"/>
                 <CampoTexto label="Cargo" placeholder="Digite seu cargo"/>
                 <CampoTexto label="Imagem" placeholder="Informe o endereço da imagem"/>
+                <ListaSuspensa label="Time" itens={times}/>
             </form>
         </section>
     )
