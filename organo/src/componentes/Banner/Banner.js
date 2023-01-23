e54e1afdef67d2068aec0566c9e608487668e4b7
soarezlucas02 <lucassoaresesouza@gmail.com>
2023-01-23 01:52:12 -0300

    Adicionando Banner e apagando arquivos desnecessarios

---
+++ organo/src/componentes/Banner/Banner.js
@@ -0,0 +1,11 @@
+import './Banner.css'
+
+function Banner(){
+    return(
+        <header className='banner'>
+            <img src="/imagens/banner.png" alt="Banner Principal"/>
+        </header>
+    )
+}
+
+export default Banner
